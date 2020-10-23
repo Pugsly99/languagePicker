a5f7c98eba037ecb40d16176b05becfbf1843df8
Pugsly99 <rylandadams@yahoo.com>
2020-10-23 10:16:06 -0700

    add if else

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-  $("form#cartoon").submit(function(event) {
+  $("form#cslang").submit(function(event) {
     var personality = parseInt($("select#personality").val());
     var favecolor = parseInt($("select#favecolor").val());
+    var bedtime = parseInt($("select#bedtime").val());
+    var music = parseInt($("select#fmusic").val());
+    var meal = parseInt($("select#meal").val());
 
-    if (personality === 1 && favecolor === 2) {
+    var total = personality + favecolor + bedtime + music + meal;
+
+    if (total === 1 && favecolor === 2) {
       $().hide();
       $('#lang1').show();
-    } else if (personality === 1 && favecolor === 1) {
+    } else if (total === 1 && favecolor === 1) {
       $().hide();
       $('#lang2').show();
-    } else if (personality === 1 && favecolor === 3) {
+    } else (personality === 1 && favecolor === 3) {
       $().hide();
       $('#lang3').show();
     }   
